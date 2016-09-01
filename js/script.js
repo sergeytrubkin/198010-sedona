51a14293c58609aecae5243fc86957bfcb5d2143
serj555 <trubkinsa@gmail.com>
2016-09-01 20:16:53 +0300

    анимация и валидация формы

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var link = document.querySelector(".search-hotel .btn");
+var popup = document.querySelector(".search-hotel form");
+var arrival = popup.querySelector("[name=date-arrival]");
+var departure = popup.querySelector("[name=date-departure]");
+var adults = popup.querySelector("[name=adults]");
+var children = popup.querySelector("[name=children]");
+
+link.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.toggle("content-show");
+	arrival.focus();
+})
+
+popup.addEventListener("submit", function(event) {
+	if (!arrival.value || !departure.value || !adults.value || !children.value) {
+		event.preventDefault();
+		alert("Все поля формы должны быть заполнены");
+	}
+})
+
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if (popup.classList.contains("content-show")) {
+			popup.classList.remove("content-show");
+		}
+	}
+})
+
